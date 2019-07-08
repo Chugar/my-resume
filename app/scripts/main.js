cc3efc51d78033a919d3b3f64d228bc5dc3ebc7c
Chugar <c.berkane@hotmail.fr>
2019-07-09 01:18:39 +0200

    fist commit of my resume

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+let letter;
+let i = 0, pos = 0;
+const typingZone = document.querySelector('.typing-effect');
+
+const texts = [
+    'Angular', 
+    'VueJS', 
+    'Symfony', 
+    'Les Single Page Apps', 
+    'Les Design Patterns', 
+    'La Programmation Fonctionelle',
+    'Les Tests Unitaires',
+];
+
+
+
+(function type() {
+    // re-initialise la position
+    if (pos === texts.length) pos = 0;
+
+    letter = texts[pos].slice(0, ++i);
+    typingZone.textContent = letter;
+
+    if(letter.length === texts[pos].length) {
+        i=0;
+        pos++;
+    }
+
+    setTimeout(type, 150);
+})();
