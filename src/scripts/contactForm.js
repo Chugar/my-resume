ef3dbbc6bcf5b500814fad4c57e228ac09380986
chugar <c.berkane@hotmail.fr>
2020-12-10 21:35:29 +0100

    added animated button

--- src/scripts/contactForm.js
+++ src/scripts/contactForm.js
@@ -1,40 +1,69 @@
 (function () {
 
-    /**
-     * Returns true if a mail is valid
-     */
-    function validateEmail(email) {
-        const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        return regex.test(email);
-    }
+    const form = document.getElementById('contact-form');
+    const submitButton = document.getElementById('envoyer');
+    const loadingButton = document.getElementById('loading');
+    const spinner = document.querySelector('.loader');
 
-    function cleanData() {
+    const fullnameInput = document.getElementById('contact-fullname');
+    const emailInput = document.getElementById('contact-email');
+    const messageTextArea = document.getElementById('contact-message');
 
-    }
-
-    function sendMail() {
-        
-    }
+    const fullnameError = document.getElementById('fullname-error');
+    const emailError = document.getElementById('email-error');
 
 
-    // Callbacks
-    const handleSubmit = (event) => {
-        event.preventDefault();
 
-        const fullnameVal =  document.getElementById('contact-fullname').value;
-        const emailVal =  document.getElementById('contact-email').value;
-        const textVal = document.getElementById('contact-message').value;
-
-        if( !validateEmail(emailVal)) {
-            return;
+    // Validity handlers
+    const handleInvalidName = (event) => {
+        fullnameError.innerHTML = '';
+        if(fullnameInput.value === '') {
+            fullnameError.innerHTML = '* Entrez votre nom et prénom';
+        }
+        if(fullnameInput.validity.tooShort) {
+            fullnameError.innerHTML = '* Trop court';
+        }
+    };
+    
+    const handleInvalidEmail = (event) => {
+        emailError.innerHTML = '';
+        if(emailInput.validity.patternMismatch) {
+            emailError.innerHTML = '* Veuillez entrer une addresse email correcte';
+        }
+        if(emailInput.validity.valueMissing) {
+            emailError.innerHTML = '* Entrer votre addresse email';
         }
     }
 
+    
+    // Animation
+    const animateSending = (event) => {
+        submitButton.style.display = 'none';
+        loadingButton.style.display = 'block';
+        
+        setTimeout(() => {
+            spinner.style.display = 'none';
+            loadingButton.innerHTML = 'Envoyé !'
+        }, 2000)
+    }
+    
+    
+    // Submit handler
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-
+        // TODO: send to a backend service (email)
+        const fullnameVal =  fullnameInput.value;
+        const emailVal =  emailInput.value;
+        const textVal = messageTextArea.value;    
+        
+        // If form is valid
+        submitButton.addEventListener('click', animateSending)
+    }
 
     // Event Listener
-    const form = document.getElementById('contact-form');
+    fullnameInput.addEventListener('blur',handleInvalidName );
+    emailInput.addEventListener('blur', handleInvalidEmail);
     form.addEventListener('submit', handleSubmit);
   
 })();
